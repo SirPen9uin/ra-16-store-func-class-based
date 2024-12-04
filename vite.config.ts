e50eeed087b620f711b59be096df388a01492e6f
Pen9uin <sirpen9uin@gmail.com>
2024-12-04 23:17:23 +0300

    path fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ra16-store-func-class-based/',
+  base: '/ra-16-store-func-class-based/',
   plugins: [react()],
 })
